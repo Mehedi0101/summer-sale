a777e9e5c242876c796c76efba24f87e84bacb19
Mehedi0101 <mehedih2909@gmail.com>
2023-08-20 20:12:30 +0600

    fixed some bug in product cart

--- scripts/purchase-successful.js
+++ scripts/purchase-successful.js
@@ -1,7 +1,14 @@
-purchaseCompleteButton.addEventListener('click',function(){
+purchaseCompleteButton.addEventListener('click', function () {
     cartList.innerHTML = '';
+    currentPrice = 0.00;
+    discountAmount = 0.00;
     priceWithDiscount.innerHTML = '0.00';
     priceWithoutDiscount.innerHTML = '0.00';
     discount.innerHTML = '0.00';
+    purchaseButton.setAttribute('disabled', true);
+    couponButton.setAttribute('disabled', true);
+    couponInUse = false;
+    invalidText.classList.add('hidden');
+    couponUsedText.classList.add('hidden');
 })
 
