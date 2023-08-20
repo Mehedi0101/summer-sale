8cb9b07e61e4a7ef036adb12593286ea730a7644
Mehedi0101 <mehedih2909@gmail.com>
2023-08-20 12:59:58 +0600

    cart list functionalities added

---
+++ scripts/cart-product.js
@@ -0,0 +1,26 @@
+function addToCart(elements){
+    
+    // adding to the cart list
+    const productToAdd = elements.children[1].children[1].innerText;
+    const li = document.createElement('li');
+    li.innerText = productToAdd;
+    cartList.appendChild(li);
+
+    // purchase button enabling
+    purchaseButton.removeAttribute('disabled');
+
+    // total price without discount
+    const productPrice = parseFloat(elements.children[1].children[2].children[0].innerText);
+    const previousPrice = parseFloat(priceWithoutDiscount.innerHTML);
+    const currentPrice = previousPrice+productPrice;
+    priceWithoutDiscount.innerHTML = currentPrice.toFixed(2);
+
+    // coupon button enabling
+    if( currentPrice >= 200 ){
+        couponButton.removeAttribute('disabled');
+    }
+
+    // total price with discount
+    priceWithDiscount.innerHTML = currentPrice.toFixed(2);
+    console.log(productPrice);
+}
